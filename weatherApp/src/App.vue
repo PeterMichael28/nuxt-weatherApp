95d9820f5a385a30fc13acd531411fc2d56b3ff6
peter michael <enitanpeters28@gmail.com>
2024-01-17 16:51:01 +0100

    nuxt-weather-app

---
+++ weatherApp/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col min-h-full font-Roboto bg-pry" >
+    <Navigation />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import Navigation from './components/Navigation.vue'
+</script>
